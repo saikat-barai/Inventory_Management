d80066cd9f32dede7198cd95f164de27900f6cac
Saikat Barai <saikatbarai299@.com>
2025-03-22 22:35:48 +0600

    authentication page mastering

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    function userRegistration(){
+        return view('pages.auth.registration');
+    }
+
+    function userLogin(){
+        return view('pages.auth.login');
+    }
+
+    function reset_password_page(){
+        return view('pages.auth.password-reset');
+    }
+
+    function sent_OTP(){
+        return view('pages.auth.sent-otp');
+    }
+
+    function verify_OTP(){
+        return view('pages.auth.verify-otp');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,15 @@
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [HomeController::class, 'homePage'])->name('home');
 Route::get('/dashboard', [DashboardController::class, 'dashboardPage'])->name('dashboard');
 Route::get('/category', [CategoryController::class, 'categoryPage'])->name('category');
+Route::get('/userRegistration', [UserController::class, 'userRegistration'])->name('userRegistration');
+Route::get('/userLogin', [UserController::class, 'userLogin'])->name('userLogin');
+Route::get('/reset/password', [UserController::class, 'reset_password_page'])->name('reset.password');
+Route::get('/sent/OTP', [UserController::class, 'sent_OTP'])->name('sent.OTP');
+Route::get('/verify/OTP', [UserController::class, 'verify_OTP'])->name('verify.OTP');
